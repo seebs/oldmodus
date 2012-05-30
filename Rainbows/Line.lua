167dc9713f532c7b016b30354ad270f14f160b2b
Seebs <seebs@seebs.net>
2012-05-30 12:21:44 -0500

    line optimizations

--- Rainbows/Line.lua
+++ Rainbows/Line.lua
@@ -4,6 +4,16 @@ local frame = Util.line
 local max = math.max
 local min = math.min
 local deg = math.deg
+local sqrt = math.sqrt
+local atan2 = math.atan2
+local printf = Util.printf
+
+local redrawn = 0
+
+function Line.redraws()
+  printf("redrawn: %d", redrawn)
+  redrawn = 0
+end
 
 function Line.new(x1, y1, x2, y2, depth, r, g, b)
   local p1, p2
@@ -48,7 +58,14 @@ function Line.new(x1, y1, x2, y2, depth, r, g, b)
   o.setAlpha = Line.setAlpha
   o.setTheta = Line.setTheta
   o.setColor = Line.setColor
-  o.redraw = Line.redraw
+  if o.depth == 1 then
+    o.redraw = Line.redraw1
+  elseif o.depth == 2 then
+    o.redraw = Line.redraw2
+  else
+    o.redraw = Line.redraw
+  end
+  o.dirty = true
   return o
 end
 
@@ -58,11 +75,13 @@ end
 
 function Line:setTheta(theta)
   self.theta = theta
+  self.dirty = true
 end
 
 function Line:setPoints(p1, p2)
   self.p1 = { x = p1.x, y = p1.y }
   self.p2 = { x = p2.x, y = p2.y }
+  self.dirty = true
 end
 
 function Line:setThickness(thickness)
@@ -77,15 +96,41 @@ function Line:setColor(r, g, b, a)
   self.r, self.g, self.b, self.a = r, g, b, (a or 255)
 end
 
-function Line:redraw()
-  local f = frame(self.p1, self.p2)
-  self.x, self.y = f.x, f.y
-  for i, l in ipairs(self.lines) do
-    l.xScale = f.len + .0001
-    l:setFillColor(self.r, self.g, self.b, self.a)
-    l.blendMode = 'add'
+function Line:redraw1()
+  if self.dirty then
+    local ax, bx, ay, by = self.p1.x, self.p2.x, self.p1.y, self.p2.y
+    local dx, dy = ax - bx, ay - by
+    self.x, self.y, self.len, self.rotation, self.dirty = (ax + bx) / 2, (ay + by) / 2, sqrt(dx * dx + dy * dy), deg(atan2(dy, dx)) + self.theta, false
   end
-  self.rotation = deg(f.theta) + self.theta
+  self.lines[1]:setFillColor(self.r, self.g, self.b, self.a)
+  self.lines[1].blendMode, self.lines[1].xScale = 'add', self.len + .001
+  redrawn = redrawn + 1
+end
+
+function Line:redraw2()
+  if self.dirty then
+    local ax, bx, ay, by = self.p1.x, self.p2.x, self.p1.y, self.p2.y
+    local dx, dy = ax - bx, ay - by
+    self.x, self.y, self.len, self.rotation, self.dirty = (ax + bx) / 2, (ay + by) / 2, sqrt(dx * dx + dy * dy), deg(atan2(dy, dx)) + self.theta, false
+  end
+  self.lines[1]:setFillColor(self.r, self.g, self.b, self.a)
+  self.lines[1].blendMode, self.lines[1].xScale = 'add', self.len + .001
+  self.lines[2]:setFillColor(self.r, self.g, self.b, self.a)
+  self.lines[2].blendMode, self.lines[2].xScale = 'add', self.len + 1.001
+  redrawn = redrawn + 1
+end
+
+function Line:redraw()
+  if self.dirty then
+    local ax, bx, ay, by = self.p1.x, self.p2.x, self.p1.y, self.p2.y
+    local dx, dy = ax - bx, ay - by
+    self.x, self.y, self.len, self.rotation, self.dirty = (ax + bx) / 2, (ay + by) / 2, sqrt(dx * dx + dy * dy), deg(atan2(dy, dx)) + self.theta, false
+  end
+  for i, l in ipairs(self.lines) do
+    l:setFillColor(self.r, self.g, self.b, self.a)
+    l.blendMode, l.xScale = 'add', self.len + i - 0.999
+  end
+  redrawn = redrawn + 1
 end
 
 return Line
